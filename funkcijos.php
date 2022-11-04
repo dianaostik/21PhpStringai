53467d79622e608adffbf6689d4b8c692b9472b1
dianaostik <dianastud12@gmail.com>
2022-11-04 17:03:35 +0200

    funkcijos 1 ir 2

--- funkcijos.php
+++ funkcijos.php
@@ -2,12 +2,28 @@
 <?php
 
 echo"<h2> -------- Funkcijos ------------ </h2>";
-echo"<p> -------- 1 uzduotis ------------ </p>";
-        function writeMsg($fname) {
-            echo "<h1> $fname </h1>";
+echo"<p> -------- 1 ir 2 uzduotis ------------ </p>";
+        function writeMsg($fname, $ftag) {
+            echo ("<h" . $ftag . ">" . $fname . "</h" . $ftag . ">");
         }
-        
-        writeMsg("Programavimas");
+            
+        writeMsg("Programavimas", "1");
+
+
+echo"<p> -------- 3 uzduotis ------------ </p>";
+    //  $rndStr = md5(time()); 
+    //  echo $rndStr; 
+    //     echo "<br>";  
+    //     if (true) {
+    //        $rndStr1 = preg_replace_callback('/\D/', '', $rndStr);
+    //         echo ("<h1>" . $rndStr1 . "</h1>"); 
+    //     } else {
+    //         echo ("<P>" . $rndStr ."</p>");
+    //     }
+    
+    // $rndStr1 = preg_split('/(?<=[0-9])(?=[a-z]+)/i',$rndStr);   
+    // echo("<br>");                                  
+    // echo json_encode($rndStr1);
 
 
 
